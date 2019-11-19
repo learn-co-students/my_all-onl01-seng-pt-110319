b85d83d6cdc9af9c20462724f4ea125448b1d436
qiong sun <joannesun44@gmail.com>
2019-11-19 22:05:03 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,9 @@ def my_all?(collection)
 i = 0 
 collect = []
 while i < collection.length 
-collect << yield (collection [i])
+collect << yield(collection [i])
 i += 1  
+
 end 
 
 if collect.include? (false)
