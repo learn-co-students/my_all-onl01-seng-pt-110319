90d11e14ce7522862f07a545d90f4932d7c5c5c3
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-11-21 15:30:20 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(array)
+  i = 0
+  return_values = []
+  while i < array.length
+    return_values << yield(array[i])
+    # yield(array[i])
+    i = i + 1
+  end
+  # binding.pry
+  if return_values.include?(false)
+    false
+  else
+    true
+  end
 end
